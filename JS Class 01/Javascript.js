eadfdee168dabe7335c6e31eeb3715b866e3fbd2
SYED FAIZ AHMED <64730518+sfa129@users.noreply.github.com>
2023-06-05 09:50:27 +0500

    Add files via upload

---
+++ JS Class 01/Javascript.js
@@ -0,0 +1,29 @@
+//alert("Hello World");
+
+// var a = 5;
+// var b = 6;
+// var c = a+b;
+// alert(c);
+
+
+// var a = "Pakistan is";
+// var b = 01;
+// var c = a+b;
+// alert(c);
+
+// var a = 4;
+// var b = a++
+// alert(b)
+// //answer is 4 because "a++"having post increment
+
+// var a = 4;
+// var b = ++a;
+// alert(b)
+// //answer is 5 because increment value in start
+
+// var x = 10;
+// var y = x++ + ++x - x + x++ - ++x + x;
+// //      10 +  12  - 12+ 12  - 14  + 14
+// //      22
+// var z = alert(y);
+
