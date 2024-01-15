41dbe5845ace5f9c7bb6db7b48d30377c2a6f9c3
SYED FAIZ AHMED <64730518+sfa129@users.noreply.github.com>
2024-01-15 11:38:28 +0500

    Styling with DOM
    In this file, we make styling changes by targeting the elements by DOM manipulation.

---
+++ JS Class 16.1/app.js
@@ -0,0 +1,29 @@
+
+//  change the size of an image 
+// function bigImage(){
+//     var bimg = document.getElementById('img');
+//     bimg.className="big"
+// }
+
+// function smallImage(){
+//     var simg = document.getElementById('img');
+//     simg.className="small"
+// }
+
+// //Apply styling on paragraph
+// function applyStyle(){
+//     var simg = document.getElementById('para');
+//     simg.className='styleofimage'
+// }
+
+// Apply styling with multiple classes
+// function applyStyle(){
+//     var simg = document.getElementById('para');
+//     simg.className +='styleofimage colr'
+// }
+
+// target all tags (not working)
+// function tagAll(){
+//     var allTag = document.getElementsByTagName(p);
+//     allTag.style.color="red"
+// }
