fe2bcf6f15e364be486929f2d3c4a74d28a10756
SYED FAIZ AHMED <64730518+sfa129@users.noreply.github.com>
2024-01-16 11:28:37 +0500

    Simple Messaging
    It contains the code about the simple messaging on browser / console

---
+++ JS Class 18/app.js
@@ -0,0 +1,28 @@
+
+
+
+
+//aik js file recycle bin mein hai woh restore karni hai
+
+//without HTML, write the text
+//but not working
+// var p = document.createElement('p');
+// var text = document.createTextNode('Hello World!')
+
+// p.appendChild(text)
+
+// var main = document.getElementById('main');
+// main.appendChild(p)
+
+//simple messaging
+// function addMessages(){
+//     var val = document.getElementById("val");
+//     var p = document.createElement("p")
+//     var text = document.createTextNode(val.value)
+//     p.appendChild(text)
+//     var messages = document.getElementById("messages")
+//     messages.appendChild(p)
+
+//     val.value = ''
+// }
+
